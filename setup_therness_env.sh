ce2035a2ee058236eef792d49f1d8931f2d5402e
AndreaLolli2912 <andrea.lolli1@outlook.com>
2026-02-27 15:37:14 +0100

    fix: change env name

--- setup_therness_env.sh
+++ setup_therness_env.sh
@@ -1,4 +1,4 @@
-conda create -n torch311 python=3.11 -y
+conda create -n therness_env python=3.11 -y
 conda activate torch311
 pip install --upgrade pip
 pip install --index-url https://download.pytorch.org/whl/cu128 torch torchvision
